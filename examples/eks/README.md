e6a545e10b2318cd76788fee629d4d5554000def
zamboni86 <zanoni.contreras@gmail.com>
2020-10-17 09:04:19 -0600

    change module to aws provisioner

---
+++ examples/eks/README.md
@@ -0,0 +1,6 @@
+# Elastic Kubernetes Service (EKS)
+Provisons EKS cluster. Creates the following resources:
+- Role and attached AWS managed EKS policy
+- EKS cluster
+- Compute node(s)
+
--- examples/eks/main.tf
+++ examples/eks/main.tf
@@ -1,20 +1,76 @@
-locals {
-  environment = "example"
-  cluster_name = "example"
+########################################
+# eks role and attached policy
+########################################
+
+resource "aws_iam_role" "eks-role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
 }
 
-module "my-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-cluster"
-  cluster_version = "1.17"
-  subnets         = ["subnet-003bd54b5bb33cec6", "subnet-00bcbaa398cb71ace"]
-  vpc_id          = "vpc-0e0f2c2d2a6f5bfb8"
+resource "aws_iam_role_policy_attachment" "eks-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-role.name
+}
 
-  worker_groups = [
-    {
-      instance_type = "t3.small"
-      asg_max_size  = 2
-      asg_desired_capacity = 2
-    }
+########################################
+# eks cluster definition
+########################################
+
+resource "aws_eks_cluster" "eks" {
+  name     = "example"
+  role_arn = aws_iam_role.example.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-0d4557868c7d7c53f", "subnet-0c7eabd0b511ffb7a"]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-role
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
+}
+
+########################################
+# compute node definition
+########################################
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "example"
+  node_role_arn   = aws_iam_role.example.arn
+  subnet_ids      = aws_subnet.example[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
