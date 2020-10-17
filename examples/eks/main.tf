e82fe1bb4cf885691efbb7c6356567722200f677
zamboni86 <zanoni.contreras@gmail.com>
2020-10-17 09:46:12 -0600

    add compute node to EKS cluster

--- examples/eks/main.tf
+++ examples/eks/main.tf
@@ -1,8 +1,9 @@
 ########################################
-# eks role and attached policy
+# cluster resource
 ########################################
 
-resource "aws_iam_role" "eks-role" {
+# eks role
+resource "aws_iam_role" "eks-cluster" {
   name = "eks-cluster-role"
 
   assume_role_policy = <<POLICY
@@ -23,42 +24,72 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "eks-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-role.name
+  role       = aws_iam_role.eks-cluster.name
 }
 
-########################################
 # eks cluster definition
-########################################
-
 resource "aws_eks_cluster" "eks" {
   name     = "example"
-  role_arn = aws_iam_role.example.arn
+  role_arn = aws_iam_role.eks-cluster.arn
 
   vpc_config {
     subnet_ids = ["subnet-0d4557868c7d7c53f", "subnet-0c7eabd0b511ffb7a"]
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks-role
+    aws_iam_role_policy_attachment.eks-policy
   ]
 }
 
 output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
+  value = aws_eks_cluster.eks.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
+  value = aws_eks_cluster.eks.certificate_authority[0].data
 }
 
 ########################################
-# compute node definition
+# node resource(s)
 ########################################
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "example"
-  node_role_arn   = aws_iam_role.example.arn
-  subnet_ids      = aws_subnet.example[*].id
+
+# node role
+resource "aws_iam_role" "node" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node.name
+}
+
+# compute node definition
+resource "aws_eks_node_group" "node" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "example-node"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = ["subnet-0d4557868c7d7c53f", "subnet-0c7eabd0b511ffb7a"]
 
   scaling_config {
     desired_size = 1
@@ -69,8 +100,8 @@ resource "aws_eks_node_group" "example" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
